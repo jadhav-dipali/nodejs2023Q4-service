d6f295c3aa743b8de9bb05b2108789bea7c0bcd5
jadhav-dipali <jadhavdipali924@gmail.com>
2023-04-14 13:16:42 +0530

    File is updated and its finally done

--- src/router/Favorites.js
+++ src/router/Favorites.js
@@ -3,18 +3,9 @@ const FavoritesRouter = new express.Router();
 const Favorites = require("../model/FavoritesSchema")
 const defaultId="dmsklcmflermvlerm";
 
-FavoritesRouter.get("/favs" , async(req,res)=>{
-    try {
-       let data =  await Favorites.find();
-       res.status(200).json({message:"success",data}) 
-    } catch (error) {
-        res.status(400).send({message:error.message})
-    }
-})
 
-
-FavoritesRouter.post("/favs/track/:id" , async(req,res)=>{
-    try {
+async function isFav(req,res,next){
+    try{
         let user = await Favorites.findById(defaultId)
         if(!user){
           user= await new Favorites({
@@ -25,11 +16,33 @@ FavoritesRouter.post("/favs/track/:id" , async(req,res)=>{
           })
           await user.save()
         }
-        if(user){
-            if(user.tracks)
-           await Favorites.findByIdAndUpdate(defaultId,{$push:{tracks:req.params.id}}) 
-           res.status(201).json({status:"Success" , message:"New track Add in Favorites"})
+    }catch(err){
+        res.status(400).json({message:err.message})
+    }
+    next();
+}
+
+FavoritesRouter.get("/favs" , async(req,res)=>{
+    try {
+       let data =  await Favorites.find();
+       res.status(200).json({message:"success",data}) 
+    } catch (error) {
+        res.status(400).send({message:error.message})
+    }
+})
+
+
+FavoritesRouter.post("/favs/track/:id" , isFav, async(req,res)=>{
+    try {
+        let user = await Favorites.findById(defaultId)
+        if(user.tracks.find((a)=>{return (a===req.params.id)})===undefined){
+            await Favorites.findByIdAndUpdate(defaultId,{$push:{tracks:req.params.id}}) 
+            res.status(201).json({status:"Success" , message:"New track Add in Favorites"})
+        }else{
+            res.status(400).json({status : "fail" , message:"Id Already Add in Favorites" })
         }
+       
+        
     } catch (error) {
         res.status(400).send({message:error.message})
         
@@ -46,22 +59,15 @@ FavoritesRouter.delete("/favs/track/:id" , async(req,res)=>{
 })
 
 
-FavoritesRouter.post("/favs/artist/:id" , async(req,res)=>{
+FavoritesRouter.post("/favs/artist/:id" ,isFav, async(req,res)=>{
     try {
         let user = await Favorites.findById(defaultId)
-        if(!user){
-          user= await new Favorites({
-            _id:defaultId,
-            artists:[],
-            albums:[],
-            tracks:[]
-          })
-          await user.save()
-        }
-        if(user){
-           await Favorites.findByIdAndUpdate(defaultId,{$push:{artists:req.params.id}}) 
-           res.status(201).json({status:"Success" , message:"New Artists Add in Favorites"})
-        }
+        if(user.artists.find((a)=>{return (a===req.params.id)})===undefined){
+            await Favorites.findByIdAndUpdate(defaultId,{$push:{artists:req.params.id}}) 
+            res.status(201).json({status:"Success" , message:"New Artists Add in Favorites"})
+        }else{
+            res.status(400).json({status : "fail" , message:"Id Already Add in Favorites" })
+        }  
     } catch (error) {
         res.status(400).send({message:error.message})
         
@@ -78,22 +84,17 @@ FavoritesRouter.delete("/favs/artist/:id" , async(req,res)=>{
 })
 
 
-FavoritesRouter.post("/favs/album/:id" , async(req,res)=>{
+FavoritesRouter.post("/favs/album/:id" ,isFav, async(req,res)=>{
     try {
+      
         let user = await Favorites.findById(defaultId)
-        if(!user){
-          user= await new Favorites({
-            _id:defaultId,
-            artists:[],
-            albums:[],
-            tracks:[]
-          })
-          await user.save()
-        }
-        if(user){
-           await Favorites.findByIdAndUpdate(defaultId,{$push:{albums:req.params.id}}) 
-           res.status(201).json({status:"Success" , message:"New Album Add in Favorites"})
-        }
+        if(user.albums.find((a)=>{return (a===req.params.id)})===undefined){
+            await Favorites.findByIdAndUpdate(defaultId,{$push:{albums:req.params.id}}) 
+            res.status(201).json({status:"Success" , message:"New Album Add in Favorites"}) 
+        }else{
+            res.status(400).json({status : "fail" , message:"Id Already Add in Favorites" })
+        }  
+           
     } catch (error) {
         res.status(400).send({message:error.message})
         
